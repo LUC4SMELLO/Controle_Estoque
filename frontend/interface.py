38d5b7f183aaf66a8e5cdab3a6f354581e521775
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-07-05 13:10:43 -0300

    adicionando botao de produto

--- frontend/interface.py
+++ frontend/interface.py
@@ -1,5 +1,19 @@
+import tkinter as tk
+
 from frontend.janela import janela
 
+from frontend.menus.produtos_menus import mostrar_botoes_produtos
+        
+def mostrar_frame(frame):
+    frame.place(x=100, y=10)
+
 def inicializar_interface():
+
+    botao_produtos = tk.Button(janela, text="Produtos", font=("Arial", 15, "bold"), command=mostrar_botoes_produtos)
+    botao_produtos.place(x=10, y=10)
+
     
+
+
+
     janela.mainloop()
---
+++ frontend/menus/produtos_menu.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+from frontend.janela import janela
+
+botao_cadastrar_produto = tk.Button(janela, text="Cadastrar", font=("Arial", 10, "bold"))
+botao_alterar_produto = tk.Button(janela, text="Alterar", font=("Arial", 10, "bold"))
+botao_excluir_produto = tk.Button(janela, text="Excluir", font=("Arial", 10, "bold"))   
+
+botoes_sendo_mostrados = False
+
+def mostrar_botoes_produtos():
+    global botoes_sendo_mostrados
+
+    if botoes_sendo_mostrados:
+        botao_cadastrar_produto.place_forget()
+        botao_alterar_produto.place_forget()
+        botao_excluir_produto.place_forget()
+        botoes_sendo_mostrados = False
+    else:
+        botao_cadastrar_produto.place(x=50, y=55)
+        botao_alterar_produto.place(x=50, y=85)
+        botao_excluir_produto.place(x=50, y=115)
+        botoes_sendo_mostrados = True
