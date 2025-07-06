4f3a0b904f4a530df821bff7362e7f73963fca37
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-07-05 23:34:25 -0300

    corrigido bug na visualização dos botões

--- frontend/menus/menu_estoque.py
+++ frontend/menus/menu_estoque.py
@@ -1,30 +1,59 @@
 import tkinter as tk
 from frontend.janela import janela
 
+import frontend.menus.menu_produtos as menu_produtos
+
 botao_nota_fiscal_estoque = tk.Button(janela, text="Nota Fiscal", font=("Arial", 10, "bold"))
 botao_entrada_estoque = tk.Button(janela, text="Entrada", font=("Arial", 10, "bold"))
 botao_saida_estoque = tk.Button(janela, text="Saída", font=("Arial", 10, "bold"))
 
-botoes_sendo_mostrados = False
+botoes_sendo_mostrados_estoque = False
+espacamento = 90
+
 def mostrar_botoes_estoque(botao_apartados):
 
-    global botoes_sendo_mostrados
+    global botoes_sendo_mostrados_estoque
 
-    if botoes_sendo_mostrados:
+
+    if menu_produtos.botoes_sendo_mostrados_produtos and botoes_sendo_mostrados_estoque:
         botao_nota_fiscal_estoque.place_forget()
         botao_entrada_estoque.place_forget()
         botao_saida_estoque.place_forget()
 
-        botoes_sendo_mostrados = False
+        botoes_sendo_mostrados_estoque = False
+
+        botao_apartados.place(x=10, y=200)
+        return
+    
+    if botoes_sendo_mostrados_estoque:
+        botao_nota_fiscal_estoque.place_forget()
+        botao_entrada_estoque.place_forget()
+        botao_saida_estoque.place_forget()
+
+        botoes_sendo_mostrados_estoque = False
 
         botao_apartados.place(x=10, y=110)
-        
+        return
+    
+    if menu_produtos.botoes_sendo_mostrados_produtos:
+        botao_nota_fiscal_estoque.place(x=50, y=105 + 90)
+        botao_entrada_estoque.place(x=50, y=135 + 90)
+        botao_saida_estoque.place(x=50, y=165 + 90)
 
-    else:
+        botoes_sendo_mostrados_estoque = True
+
+        botao_apartados.place(x=10, y=290)
+        return
+    
+    
+
+    
+    if botoes_sendo_mostrados_estoque == False:
         botao_nota_fiscal_estoque.place(x=50, y=105)
         botao_entrada_estoque.place(x=50, y=135)
         botao_saida_estoque.place(x=50, y=165)
 
-        botoes_sendo_mostrados = True
+        botoes_sendo_mostrados_estoque = True
 
         botao_apartados.place(x=10, y=200)
+        return
--- frontend/menus/menu_produtos.py
+++ frontend/menus/menu_produtos.py
@@ -11,16 +11,16 @@ botao_excluir_produto = tk.Button(janela, text="Excluir", font=("Arial", 10, "bo
 
 
 
-botoes_sendo_mostrados = False
+botoes_sendo_mostrados_produtos = False
 
 def mostrar_botoes_produtos(botao_estoque, botao_apartados):
-    global botoes_sendo_mostrados
+    global botoes_sendo_mostrados_produtos
 
-    if botoes_sendo_mostrados:
+    if botoes_sendo_mostrados_produtos:
         botao_cadastrar_produto.place_forget()
         botao_alterar_produto.place_forget()
         botao_excluir_produto.place_forget()
-        botoes_sendo_mostrados = False
+        botoes_sendo_mostrados_produtos = False
 
         botao_estoque.place(x=10, y=60)
         botao_apartados.place(x=10, y=110)
@@ -29,7 +29,7 @@ def mostrar_botoes_produtos(botao_estoque, botao_apartados):
         botao_cadastrar_produto.place(x=50, y=55)
         botao_alterar_produto.place(x=50, y=85)
         botao_excluir_produto.place(x=50, y=115)
-        botoes_sendo_mostrados = True
+        botoes_sendo_mostrados_produtos = True
 
         botao_estoque.place(x=10, y=150)
         botao_apartados.place(x=10, y=200)
