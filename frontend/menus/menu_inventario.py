458ff2cc4187a87e64effb8d3f5e332f8eaf0d02
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-07-07 15:47:28 -0300

    adicionando novos botões para o menu_inventario

--- frontend/menus/menu_inventario.py
+++ frontend/menus/menu_inventario.py
@@ -9,9 +9,10 @@ def criar_botao_inventario(janela_pai):
     botao_inventario = tk.Button(janela_pai, text="Inventário", font=("Arial", 15, "bold"), command=lambda: organizar_botoes("inventario"))
     botao_inventario.place(x=10, y=310)
 
-    botao_digitar_estoque_inventario = tk.Button(janela_pai, text="Digitação", font=("Arial", 10, "bold"))
+    botao_digitar_produtos_inventario = tk.Button(janela_pai, text="Digitação Produtos", font=("Arial", 10, "bold"))
+    botao_digitar_doces_inventario = tk.Button(janela_pai, text="Digitação Doces", font=("Arial", 10, "bold"))
+    botao_ajustar_diferencas_inventario = tk.Button(janela_pai, text="Ajustar Diferenças", font=("Arial", 10, "bold"))
     botao_comparar_estoque_inventario = tk.Button(janela_pai, text="Comparar Estoque", font=("Arial", 10, "bold"))
-    botao_doces_alimentos_inventario = tk.Button(janela_pai, text="Doces Alimentos", font=("Arial", 10, "bold"))
 
     state.botoes_principais["inventario"] = botao_inventario
-    state.botoes_submenus["inventario"] = [botao_digitar_estoque_inventario, botao_doces_alimentos_inventario, botao_comparar_estoque_inventario,]
+    state.botoes_submenus["inventario"] = [botao_digitar_produtos_inventario, botao_digitar_doces_inventario, botao_ajustar_diferencas_inventario, botao_comparar_estoque_inventario,]
