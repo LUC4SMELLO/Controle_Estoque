972cf217fe9a66ed044e8753006eca84f490da8b
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-07-07 16:21:51 -0300

    adicionando botao 'entradas saidas' no menu relatorios

--- frontend/menus/menu_relatorios.py
+++ frontend/menus/menu_relatorios.py
@@ -10,9 +10,10 @@ def criar_botao_relatorios(janela_pai):
     botao_relatorios.place(x=10, y=260)
 
     botao_produtos_relatorios = tk.Button(janela_pai, text="Produtos", font=("Arial", 10, "bold"))
+    botao_entradas_saidas_relatorios = tk.Button(janela_pai, text="Entradas e Saídas", font=("Arial", 10, "bold"))
     botao_validade_relatorios = tk.Button(janela_pai, text="Validade", font=("Arial", 10, "bold"))
     botao_lotes_relatorios = tk.Button(janela_pai, text="Lotes", font=("Arial", 10, "bold"))
 
 
     state.botoes_principais["relatorios"] = botao_relatorios
-    state.botoes_submenus["relatorios"] = [botao_produtos_relatorios, botao_validade_relatorios, botao_lotes_relatorios]
+    state.botoes_submenus["relatorios"] = [botao_produtos_relatorios, botao_entradas_saidas_relatorios, botao_validade_relatorios, botao_lotes_relatorios]
