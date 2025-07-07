3e058b6f2e4de731f5b749e268928d1879b1a412
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-07-07 13:49:33 -0300

    adicionando menu de relatórios

--- frontend/interface.py
+++ frontend/interface.py
@@ -7,6 +7,7 @@ from frontend.menus.menu_estoque import criar_botao_estoque
 from frontend.menus.menu_apartados import criar_botao_apartados
 from frontend.menus.menu_pendencias import criar_botao_pendencias
 from frontend.menus.menu_movimentações import criar_botao_movimentacoes
+from frontend.menus.menu_relatorios import criar_botao_relatorios
 
 def mostrar_frame(frame):
     frame.place(x=100, y=10)
@@ -17,6 +18,7 @@ def inicializar_interface():
     criar_botao_apartados(janela)
     criar_botao_pendencias(janela)
     criar_botao_movimentacoes(janela)
+    criar_botao_relatorios(janela)
 
 
 
---
+++ frontend/menus/menu_relatorios.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+from backend.menu_controller import organizar_botoes
+
+import frontend.menu_state as state
+
+def criar_botao_relatorios(janela_pai):
+
+    botao_relatorios = tk.Button(janela_pai, text="Relatórios", font=("Arial", 15, "bold"), command=lambda: organizar_botoes("relatorios"))
+    botao_relatorios.place(x=10, y=260)
+
+    botao_produtos_relatorios = tk.Button(janela_pai, text="Produtos", font=("Arial", 10, "bold"))
+    botao_validade_relatorios = tk.Button(janela_pai, text="Validade", font=("Arial", 10, "bold"))
+    botao_lotes_relatorios = tk.Button(janela_pai, text="Lotes", font=("Arial", 10, "bold"))
+
+
+    state.botoes_principais["relatorios"] = botao_relatorios
+    state.botoes_submenus["relatorios"] = [botao_produtos_relatorios, botao_validade_relatorios, botao_lotes_relatorios]
