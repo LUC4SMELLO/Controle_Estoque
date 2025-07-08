04b96b1540d538d0e7f06ae3b98c6742d6047069
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-07-08 10:48:11 -0300

    adicionando a subjanela para cadastro de produto

---
+++ frontend/janelas/produtos/cadastrar_produto.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+def criar_janela_cadastrar_produto():
+
+    janela_cadastrar_produtos = tk.Toplevel()
+    janela_cadastrar_produtos.title("Cadastrar Produto")
+    janela_cadastrar_produtos.geometry("800x600")
--- frontend/menus/menu_produto.py
+++ frontend/menus/menu_produto.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
 from backend.menu_controller import organizar_botoes
+from frontend.janelas.produtos.cadastrar_produto import criar_janela_cadastrar_produto
 
 import frontend.menu_state as state
 
@@ -9,7 +10,7 @@ def criar_botao_produtos(janela_pai):
     botao_produtos = tk.Button(janela_pai, text="Produtos", font=("Arial", 15, "bold"), width=13, command=lambda: organizar_botoes("produtos"))
     botao_produtos.place(x=10, y=10)
 
-    botao_cadastrar_produto = tk.Button(janela_pai, text="Cadastrar", font=("Arial", 10, "bold"))
+    botao_cadastrar_produto = tk.Button(janela_pai, text="Cadastrar", font=("Arial", 10, "bold"), command=criar_janela_cadastrar_produto)
     botao_alterar_produto = tk.Button(janela_pai, text="Alterar", width=8, font=("Arial", 10, "bold"))
     botao_excluir_produto = tk.Button(janela_pai, text="Excluir", width=8, font=("Arial", 10, "bold"))
     botao_consultar_produto = tk.Button(janela_pai, text="Consultar", width=8, font=("Arial", 10, "bold"))
