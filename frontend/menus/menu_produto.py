5b2b268bf08c2e0fe7c7b724cc34439ffe55b350
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-07-09 16:15:27 -0300

    janela de excluir produtos finalizada

--- frontend/menus/menu_produto.py
+++ frontend/menus/menu_produto.py
@@ -4,6 +4,7 @@ from backend.menu_controller import organizar_botoes
 
 from frontend.janelas.produtos.cadastrar_produto import criar_janela_cadastrar_produto
 from frontend.janelas.produtos.alterar_produto import criar_janela_alterar_produto
+from frontend.janelas.produtos.excluir_produto import criar_janela_excluir_produto
 
 import frontend.menu_state as state
 
@@ -14,7 +15,7 @@ def criar_botao_produtos(janela_pai):
 
     botao_cadastrar_produto = tk.Button(janela_pai, text="Cadastrar", font=("Arial", 10, "bold"), command=criar_janela_cadastrar_produto)
     botao_alterar_produto = tk.Button(janela_pai, text="Alterar", width=8, font=("Arial", 10, "bold"), command=criar_janela_alterar_produto)
-    botao_excluir_produto = tk.Button(janela_pai, text="Excluir", width=8, font=("Arial", 10, "bold"))
+    botao_excluir_produto = tk.Button(janela_pai, text="Excluir", width=8, font=("Arial", 10, "bold"), command=criar_janela_excluir_produto)
     botao_consultar_produto = tk.Button(janela_pai, text="Consultar", width=8, font=("Arial", 10, "bold"))
 
     state.botoes_principais["produtos"] = botao_produtos
