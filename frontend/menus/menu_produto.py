8cc82f079b1befa33d379a0d82b8abb219584632
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-07-09 16:06:31 -0300

    janela de alterar produtos finalizada

--- frontend/menus/menu_produto.py
+++ frontend/menus/menu_produto.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 
 from backend.menu_controller import organizar_botoes
+
 from frontend.janelas.produtos.cadastrar_produto import criar_janela_cadastrar_produto
+from frontend.janelas.produtos.alterar_produto import criar_janela_alterar_produto
 
 import frontend.menu_state as state
 
@@ -11,7 +13,7 @@ def criar_botao_produtos(janela_pai):
     botao_produtos.place(x=10, y=10)
 
     botao_cadastrar_produto = tk.Button(janela_pai, text="Cadastrar", font=("Arial", 10, "bold"), command=criar_janela_cadastrar_produto)
-    botao_alterar_produto = tk.Button(janela_pai, text="Alterar", width=8, font=("Arial", 10, "bold"))
+    botao_alterar_produto = tk.Button(janela_pai, text="Alterar", width=8, font=("Arial", 10, "bold"), command=criar_janela_alterar_produto)
     botao_excluir_produto = tk.Button(janela_pai, text="Excluir", width=8, font=("Arial", 10, "bold"))
     botao_consultar_produto = tk.Button(janela_pai, text="Consultar", width=8, font=("Arial", 10, "bold"))
 
