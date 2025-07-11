6acfd0ab4f06aaf599fa0ff601a99d84c29e77cf
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-07-10 21:39:30 -0300

    funcao para atualizar informacoes no banco de dados produtos

--- models/produto.py
+++ models/produto.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from database.banco_dados_produtos import conectar_banco_de_dados_produtos
 
@@ -55,7 +55,7 @@ class Produto:
         cursor = conexao.cursor()
 
         cursor.execute(
-        """
+            """
         INSERT INTO TabelaProdutos (codigo_produto,
         descricao,
         subdescricao,
@@ -85,3 +85,91 @@ class Produto:
                 itens_lastro,
             ),
         )
+
+        conexao.commit()
+        conexao.cursor()
+
+    def atualizar_produto(
+        self,
+        codigo_produto,
+        descricao,
+        subdescricao,
+        produto_ativo,
+        unidade_medida,
+        itens_embalagem,
+        codigo_barras,
+        grupo,
+        categoria,
+        marca,
+        itens_pallete,
+        itens_lastro,
+    ):
+        conexao = conectar_banco_de_dados_produtos()
+        cursor = conexao.cursor()
+
+        cursor.execute(
+        """
+        UPDATE TabelaProdutos
+        SET descricao = ?,
+        subdescricao = ?,
+        produto_ativo = ?,
+        unidade_medida = ?,
+        itens_embalagem = ?,
+        codigo_barras = ?,
+        grupo = ?,
+        categoria = ?,
+        marca = ?,
+        itens_pallete = ?,
+        itens_lastro = ?
+        WHERE codigo_produto = ?
+        """,
+            (
+                descricao,
+                subdescricao,
+                produto_ativo,
+                unidade_medida,
+                itens_embalagem,
+                codigo_barras,
+                grupo,
+                categoria,
+                marca,
+                itens_pallete,
+                itens_lastro,
+                codigo_produto
+            )
+        )
+
+        conexao.commit()
+        conexao.cursor()
+
+cafe = Produto(
+    codigo_produto = 1324,
+    descricao = "Café da Roça",
+    subdescricao = "Tradicional Moído 500g",
+    produto_ativo = True,
+    unidade_medida = "un",
+    itens_embalagem = 20,
+    codigo_barras = "7891234567890",
+    grupo = "Mercearia",
+    categoria = "Cafés",
+    marca = "Sabor da Fazenda",
+    itens_pallete = 1200,
+    itens_lastro = 80
+)
+
+cafe.atualizar_produto(
+    codigo_produto=1324,
+    descricao="Café",
+    subdescricao = "Tradicional Moído 500g",
+    produto_ativo = True,
+    unidade_medida = "un",
+    itens_embalagem = 20,
+    codigo_barras = "7891234567890",
+    grupo = "Mercearia",
+    categoria = "Cafés",
+    marca = "Sabor da Fazenda",
+    itens_pallete = 1200,
+    itens_lastro = 80
+    )
+
+print(cafe.descricao)
