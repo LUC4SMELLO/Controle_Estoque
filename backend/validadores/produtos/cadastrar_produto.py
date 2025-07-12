aee2c8fd7773c7fe94eccdb40d0530f2bfe371d8
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-07-12 11:34:58 -0300

    validador para cadastro de produto

---
+++ backend/validadores/produtos/cadastrar_produto.py
@@ -0,0 +1,18 @@
+def validar_formulario_cadastro_produto(
+        codigo_produto,
+        descricao,
+        subdescricao,
+        unidade_medida,
+        itens_embalagem,
+        codigo_barras,
+        grupo,
+        categoria,
+        marca,
+        itens_pallete,
+        itens_lastro,
+    ):
+
+    if not codigo_produto or not descricao or not subdescricao or not unidade_medida or not itens_embalagem or not codigo_barras or not grupo or not categoria or not marca or not itens_pallete or not itens_lastro:
+        return False, "Todos os Campos Devem ser Preechidos."
+
+    return True, "Cadastro Válido."
