5c3d5c1aabffeeae1b07b58999f565afca703704
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-07-13 20:18:56 -0300

    função para alterar informações do produto

---
+++ backend/controladores/produto/alterar_controlador.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+from tkinter import messagebox
+
+from backend.models.produto import Produto
+
+
+def alterar_produto_back(
+    codigo_produto: tk.Entry,
+    descricao: tk.Entry,
+    subdescrica: tk.Entry,
+    produto_ativo: tk.BooleanVar,
+    unidade_medida: tk.Entry,
+    itens_embalagem_produtos: tk.Entry,
+    codigo_barras: tk.Entry,
+    grupo_produtos: tk.Entry,
+    categorias_produtos: tk.Entry,
+    marca_produtos: tk.Entry,
+    itens_pallete: tk.Entry,
+    itens_lastro: tk.Entry,
+):
+
+    produto = Produto(
+        codigo_produto.get().strip(),
+        descricao.get().strip(),
+        subdescrica.get().strip(),
+        produto_ativo,
+        unidade_medida.get().strip(),
+        itens_embalagem_produtos.get().strip(),
+        codigo_barras.get().strip(),
+        grupo_produtos.get().strip(),
+        categorias_produtos.get().strip(),
+        marca_produtos.get().strip(),
+        itens_pallete.get().strip(),
+        itens_lastro.get().strip(),
+    )
+    produto.atualizar_produto()
+
+    messagebox.showinfo("Sucesso!", "Produto Alterado.")
+    codigo_produto.focus_set()
+
+
+def buscar_produto_back(codigo: str):
+    
+    if not codigo.strip():
+        return False, "Preencha o Código do Produto."
+
+    resultado = Produto.buscar_produto(codigo.strip())
+
+    if not resultado:
+        return False, "Produto Não Encontrado."
+
+    return True, resultado
