c247507145c531e5611fcea73c430fb1743323ed
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-07-22 14:44:06 -0300

    colocando constantes na janela saída

--- frontend/janelas/estoque/saida.py
+++ frontend/janelas/estoque/saida.py
@@ -6,6 +6,8 @@ from backend.validadores.estoque.formulario_entrada_saida import validar_formula
 
 from backend.controladores.estoque.saida_controlador import saida_produto_back
 
+from backend.constantes.fontes import LABEL, ENTRY, BOTAO
+
 from backend.binds.configuracao_binds import configurar_binds 
 
 def criar_janela_saida_produtos():
@@ -40,37 +42,37 @@ def criar_janela_saida_produtos():
     janela_saida_produtos.geometry("400x300")
 
 
-    label_data_saida = tk.Label(janela_saida_produtos, text="Data Saída:", font=("Arial", 10, "bold"))
+    label_data_saida = tk.Label(janela_saida_produtos, text="Data Saída:", font=LABEL)
     label_data_saida.place(x=38, y=10)
 
-    entry_data_saida = tk.Entry(janela_saida_produtos, width=10, font=("Arial", 10, "bold"))
+    entry_data_saida = tk.Entry(janela_saida_produtos, width=10, font=ENTRY)
     entry_data_saida.place(x=120, y=10)
 
-    label_codigo_produto = tk.Label(janela_saida_produtos, text="Código Produto:", font=("Arial", 10, "bold"))
+    label_codigo_produto = tk.Label(janela_saida_produtos, text="Código Produto:", font=LABEL)
     label_codigo_produto.place(x=10, y=50)
 
-    entry_codigo_produto_saida = tk.Entry(janela_saida_produtos, width=10, font=("Arial", 10, "bold"))
+    entry_codigo_produto_saida = tk.Entry(janela_saida_produtos, width=10, font=ENTRY)
     entry_codigo_produto_saida.place(x=120, y=50)
 
-    label_quantidade_saida = tk.Label(janela_saida_produtos, text="Quantidade:", font=("Arial", 10, "bold"))
+    label_quantidade_saida = tk.Label(janela_saida_produtos, text="Quantidade:", font=LABEL)
     label_quantidade_saida.place(x=34, y=90)
 
-    entry_quantidade_saida = tk.Entry(janela_saida_produtos, width=10, font=("Arial", 10, "bold"))
+    entry_quantidade_saida = tk.Entry(janela_saida_produtos, width=10, font=ENTRY)
     entry_quantidade_saida.place(x=120, y=90)
 
-    label_motivo_saida = tk.Label(janela_saida_produtos, text="Motivo:", font=("Arial", 10, "bold"))
+    label_motivo_saida = tk.Label(janela_saida_produtos, text="Motivo:", font=LABEL)
     label_motivo_saida.place(x=66, y=150)
 
-    entry_motivo_saida = tk.Text(janela_saida_produtos, width=35, height=3, font=("Arial", 10, "bold"))
+    entry_motivo_saida = tk.Text(janela_saida_produtos, width=35, height=3, font=ENTRY)
     entry_motivo_saida.place(x=120, y=150)
 
     linha_horizontal_inferior = tk.Frame(janela_saida_produtos,  background="silver", width=500, height=5)
     linha_horizontal_inferior.place(x=0, y=250)
 
-    botao_confirmar_saida = tk.Button(janela_saida_produtos, text="Confirmar", font=("Arial", 10, "bold"), command=saida_produto_gui)
+    botao_confirmar_saida = tk.Button(janela_saida_produtos, text="Confirmar", font=BOTAO, command=saida_produto_gui)
     botao_confirmar_saida.place(x=200, y=265)
 
-    botao_cancelar_saida = tk.Button(janela_saida_produtos, text="Cancelar", font=("Arial", 10, "bold"))
+    botao_cancelar_saida = tk.Button(janela_saida_produtos, text="Cancelar", font=BOTAO)
     botao_cancelar_saida.place(x=300, y=265)
 
 
