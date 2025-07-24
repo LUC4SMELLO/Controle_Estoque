392b92e42b2bedc92be8b308b0827ebdc4fc25f4
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-07-24 13:44:10 -0300

    modificando função entrada produto

--- backend/controladores/estoque/entrada_controlador.py
+++ backend/controladores/estoque/entrada_controlador.py
@@ -4,9 +4,9 @@ from tkinter import messagebox
 from backend.models.produto import Produto
 
 
-def entrada_produto_back(codigo_produto: tk.Entry, quantidade_entrada: tk.Entry | int):
-    produto = Produto.buscar_produto(codigo_produto.get().strip())
-    produto.entrada_estoque_produto(int(quantidade_entrada.get().strip()))
+def entrada_produto_back(codigo_produto: str, quantidade_entrada: int):
+    produto = Produto.buscar_produto(codigo_produto)
+    produto.entrada_estoque_produto(int(quantidade_entrada))
 
     messagebox.showinfo("Sucesso!", "Entrada Concluída.")
     codigo_produto.focus_set()
