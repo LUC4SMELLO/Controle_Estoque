970a5aafc7f9b6831ebd0c2ae5513decf471c230
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-07-25 14:43:26 -0300

    começando janela cadastrar fornecedor

---
+++ frontend/janelas/fornecedor/janela_cadastrar_fornecedor.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def criar_janela_cadastrar_fornecedor():
+
+    janela_cadastrar_fornecedor = tk.Toplevel()
+    janela_cadastrar_fornecedor.title("Cadastrar Fornecedor")
+    janela_cadastrar_fornecedor.geometry("800x600")
+
+    
