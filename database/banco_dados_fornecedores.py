b03d934c043093a6061c18bf19dc1b918c395ffb
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-07-28 15:21:41 -0300

    criando banco de dados fornecedores

---
+++ database/banco_dados_fornecedores.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+def conectar_banco_de_dados_fornecedores():
+    return sqlite3.connect("TabelaFornecedores.db")
+
+def criar_tabela_produtos():
+    conexao = conectar_banco_de_dados_fornecedores()
+    cursor = conexao.cursor()
+
+    cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS TabelaFornecedores (
+        codigo_fornecedor VARCHAR(10) NOT NULL,
+        razao_social VARCHAR(100),
+        nome_fantasia VARCHAR(100),
+        fornecedor_ativo INTERGER CHECK (fornecedor_ativo IN (0, 1)),
+        cnpj VARCHAR(14),
+        inscricao_estadual VARCHAR(9),
+        logradouro VARCHAR(50),
+        bairro VARCHAR(50),
+        cidade VARCHAR(50),
+        cep VARCHAR(8),
+        estado VARCHAR(2),
+        PRIMARY KEY (codigo_fornecedor)
+    )
+    """
+    )
+
+    conexao.commit()
+    cursor.close()
