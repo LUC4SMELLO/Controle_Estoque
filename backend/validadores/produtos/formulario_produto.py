ff63e8dbc88f84247e2facf73036581db28e31f9
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-08-01 15:46:15 -0300

    adicionando validação para o código de barras produto

--- backend/validadores/produtos/formulario_produto.py
+++ backend/validadores/produtos/formulario_produto.py
@@ -14,5 +14,8 @@ def validar_formulario_produto(
 
     if not codigo_produto or not descricao or not subdescricao or not unidade_medida or not itens_embalagem or not codigo_barras or not grupo or not categoria or not marca or not itens_pallete or not itens_lastro:
         return False, "Todos os Campos Devem ser Preechidos."
+    
+    if len(codigo_barras) != 13:
+        return False, "Código de Barras Inválido."
 
     return True, "Cadastro Válido."
