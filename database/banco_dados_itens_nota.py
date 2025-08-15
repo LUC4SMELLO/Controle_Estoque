6fc93b1d8bd94e4542f2359508193edeeb34526e
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-08-15 14:50:26 -0300

    criando banco de itens notas fiscais

---
+++ database/banco_dados_itens_nota.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+def conectar_banco_de_dados_itens_nota_fiscal():
+    return sqlite3.connect("TabelaItensNotaFiscal.db")
+
+def criar_tabela_itens_nota_fiscal():
+
+    conexao = conectar_banco_de_dados_itens_nota_fiscal()
+    cursor = conexao.cursor()
+
+    cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS TabelaItensNotaFiscal.db (
+        id_item_nota VARCHAR(10),
+        numero_nota_fiscal VARCHAR(9),
+        codigo_produto VARCHAR(6),
+        quantidade VARCHAR(10),
+        preco_unitario VARCHAR(10),
+        valor_total VARCHAR(10),
+        PRIMARY KEY (id_item_nota),
+        FOREIGN KEY (numero_nota_fiscal),
+        FOREIGN KEY (codigo_produto)
+    )
+    """
+    )
+
+    conexao.commit()
+    conexao.close()
