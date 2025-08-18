04245bf9d35c51cecac10849aa34b9090177f633
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-08-18 14:35:16 -0300

    inicializando todos os bancos no app.py

--- app/main.py
+++ app/main.py
@@ -4,9 +4,11 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from frontend.interface import inicializar_interface
+from servicos.servico_banco_dados import inicializar_bancos_de_dados
 
 def main():
     inicializar_interface()
+    inicializar_bancos_de_dados()
 
 
 if __name__ == "__main__":
