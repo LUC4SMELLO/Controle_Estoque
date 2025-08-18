be72c65b88796248f8420e7088660a9e0546f5c9
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-08-18 14:33:01 -0300

    serviço para inicializar todos os banco de dados

---
+++ servicos/servico_banco_dados.py
@@ -0,0 +1,11 @@
+from database.banco_dados_produtos import criar_tabela_produtos
+from database.banco_dados_fornecedores import criar_tabela_fornecedores
+from database.banco_dados_nota_fiscal import criar_tabela_nota_fiscal
+from database.banco_dados_itens_nota import criar_tabela_itens_nota_fiscal
+
+def inicializar_bancos_de_dados():
+
+    criar_tabela_produtos()
+    criar_tabela_fornecedores()
+    criar_tabela_nota_fiscal()
+    criar_tabela_itens_nota_fiscal()
