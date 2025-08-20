0ad4fe7cfb16d9153b98db4bbc025415b5a76ec7
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-08-20 14:35:49 -0300

    modificando janela últimas compras fornecedor

--- frontend/janelas/fornecedor/janela_ultimas_compras.py
+++ frontend/janelas/fornecedor/janela_ultimas_compras.py
@@ -8,10 +8,10 @@ from backend.constantes.fontes import LABEL, ENTRY, BOTAO
 def criar_janela_ultimas_compras():
 
     janela_ultimas_compras = tk.Toplevel()
-    janela_ultimas_compras.geometry("800x600")
+    janela_ultimas_compras.geometry("1100x600")
     janela_ultimas_compras.title("Últimas Compras")
 
-    linha_horizontal_superior = tk.Frame(janela_ultimas_compras, background="silver", width=800, height=5)
+    linha_horizontal_superior = tk.Frame(janela_ultimas_compras, background="silver", width=1100, height=5)
     linha_horizontal_superior.place(x=0, y=75)
 
     label_codigo_excluir = tk.Label(janela_ultimas_compras, text="Código:", font=LABEL)
@@ -28,14 +28,14 @@ def criar_janela_ultimas_compras():
 
     entry_razao_social = tk.Entry(janela_ultimas_compras, font=ENTRY, width=60)
     entry_razao_social.place(x=115, y=40)
-
+    
     scrollbar_vertical = ttk.Scrollbar(janela_ultimas_compras, orient="vertical")
     scrollbar_vertical.place(x=1062, y=110, height=270)
 
     style = ttk.Style()
     style.configure("Treeview.Heading", font=("Arial", 10, "bold"))
 
-    colunas = ("data_ultima_compra", "numero_nota_fiscal")
+    colunas = ("data_entrada", "numero_nota_fiscal", "codigo_produto", "descricao", "quantidade", "preco_unitario", "preco_total")
     treeview_ultimas_compras = ttk.Treeview(
         janela_ultimas_compras,
         columns=colunas,
@@ -43,23 +43,34 @@ def criar_janela_ultimas_compras():
         height=12,
         yscrollcommand=scrollbar_vertical.set
     )
-    treeview_ultimas_compras.place(x=26, y=110, width=748, height=270)
+    treeview_ultimas_compras.place(x=26, y=110, width=1035, height=270)
 
     scrollbar_vertical.config(command=treeview_ultimas_compras.yview)
 
-    treeview_ultimas_compras.heading("data_ultima_compra", text="DATA ÚLTIMA COMPRA", anchor="center")
-    treeview_ultimas_compras.heading("numero_nota_fiscal", text="Nº NOTA FISCAL", anchor="center")
+    treeview_ultimas_compras.heading("data_entrada", text="DATA ENTRADA", anchor="center")
+    treeview_ultimas_compras.heading("numero_nota_fiscal", text="NOTA FISCAL", anchor="center")
+    treeview_ultimas_compras.heading("codigo_produto", text="CÓDIGO PRODUTO", anchor="center")
+    treeview_ultimas_compras.heading("descricao", text="DESCRIÇÃO", anchor="center")
+    treeview_ultimas_compras.heading("quantidade", text="QUANTIDADE", anchor="center")
+    treeview_ultimas_compras.heading("preco_unitario", text="PREÇO UNITÁRIO", anchor="center")
+    treeview_ultimas_compras.heading("preco_total", text="PREÇO TOTAL", anchor="center")
 
-    treeview_ultimas_compras.column("data_ultima_compra", anchor="center")
-    treeview_ultimas_compras.column("numero_nota_fiscal", anchor="center")
+    treeview_ultimas_compras.column("data_entrada", width=120)
+    treeview_ultimas_compras.column("numero_nota_fiscal", width=110, anchor="center")
+    treeview_ultimas_compras.column("codigo_produto", width=120, anchor="center")
+    treeview_ultimas_compras.column("descricao", width=260, anchor="center")
+    treeview_ultimas_compras.column("quantidade", width=90, anchor="center")
+    treeview_ultimas_compras.column("preco_unitario", width=120, anchor="center")
+    treeview_ultimas_compras.column("preco_total", width=120, anchor="center")
+
+    
 
 
-
-    linha_horizontal_inferior = tk.Frame(janela_ultimas_compras, background="silver", width=800, height=5)
+    linha_horizontal_inferior = tk.Frame(janela_ultimas_compras, background="silver", width=1100, height=5)
     linha_horizontal_inferior.place(x=0, y=550)
 
     botao_confirmar_excluir = tk.Button(janela_ultimas_compras, text="Confirmar", font=BOTAO)
-    botao_confirmar_excluir.place(x=600, y=565)
+    botao_confirmar_excluir.place(x=900, y=565)
 
     botao_cancelar_excluir =  tk.Button(janela_ultimas_compras, text="Cancelar", font=BOTAO)
-    botao_cancelar_excluir.place(x=700, y=565)
+    botao_cancelar_excluir.place(x=1000, y=565)
