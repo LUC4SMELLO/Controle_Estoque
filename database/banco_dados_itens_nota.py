da7e78b7d726341d01a810d0c8601b952e294a7b
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-08-20 14:31:56 -0300

    novos campos no banco itens nota fiscal

--- database/banco_dados_itens_nota.py
+++ database/banco_dados_itens_nota.py
@@ -11,13 +11,16 @@ def criar_tabela_itens_nota_fiscal():
     cursor.execute(
     """
     CREATE TABLE IF NOT EXISTS TabelaItensNotaFiscal (
+        data_entrada VARCHAR(10),
         numero_nota_fiscal VARCHAR(9),
+        codigo_fornecedor VARCHAR(6),
         codigo_produto VARCHAR(6),
         quantidade VARCHAR(10),
         preco_unitario VARCHAR(10),
         preco_total VARCHAR(10),
         FOREIGN KEY (numero_nota_fiscal) REFERENCES TabelaNotaFiscal(numero_nota_fiscal),
-        FOREIGN KEY (codigo_produto) REFERENCES TabelaProdutos(codigo_produto)
+        FOREIGN KEY (codigo_produto) REFERENCES TabelaProdutos(codigo_produto),
+        FOREIGN KEY (codigo_fornecedor) REFERENCES TabelaFornecedores(codigo_fornecedor)
     )
     """
     )
