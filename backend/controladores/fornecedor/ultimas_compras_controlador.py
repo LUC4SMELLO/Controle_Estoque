f8c7f513195575d7de3ec2473c8825326949d1e3
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-08-20 16:33:43 -0300

    função para retornar últimas compras fornecedor

---
+++ backend/controladores/fornecedor/ultimas_compras_controlador.py
@@ -0,0 +1,28 @@
+from backend.models.item_nota_fiscal import ItemNotaFiscal
+
+from database.banco_dados_itens_nota import conectar_banco_de_dados_itens_nota_fiscal
+
+def retornar_ultimas_compras_fornecedor(codigo_fornecedor):
+
+    try:
+        conexao = conectar_banco_de_dados_itens_nota_fiscal()
+        cursor = conexao.cursor()
+
+        cursor.execute(
+        """
+        SELECT * FROM TabelaItensNotaFiscal
+        WHERE codigo_fornecedor = ?
+        """, (codigo_fornecedor,)
+        )
+
+        resultado = cursor.fetchall()
+
+        conexao.commit()
+        conexao.close()
+
+        return True, ItemNotaFiscal(*resultado)
+
+
+    
+    except TypeError:
+        return False, "Nenhuma Compra Encontrada."
