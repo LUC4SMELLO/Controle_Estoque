f3c8bccf42a1efc27b441084a0ecdd4756d190d6
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-10-03 15:18:57 -0300

    mudança nomenclatura botão

--- frontend/menus/menu_inventario.py
+++ frontend/menus/menu_inventario.py
@@ -9,8 +9,8 @@ def criar_botao_inventario(janela_pai):
     botao_inventario = tk.Button(janela_pai, text="Inventário", font=("Arial", 15, "bold"), width=13, command=lambda: organizar_botoes("inventario"))
     botao_inventario.place(x=10, y=310)
 
-    botao_digitar_produtos_inventario = tk.Button(janela_pai, text="Digitação Produtos", width=15, font=("Arial", 10, "bold"))
-    botao_digitar_doces_inventario = tk.Button(janela_pai, text="Digitação Doces", width=15, font=("Arial", 10, "bold"))
+    botao_digitar_produtos_inventario = tk.Button(janela_pai, text="Contagem Produtos", width=16, font=("Arial", 10, "bold"))
+    botao_digitar_doces_inventario = tk.Button(janela_pai, text="Contagem Doces", width=15, font=("Arial", 10, "bold"))
     botao_ajustar_diferencas_inventario = tk.Button(janela_pai, text="Ajustar Diferenças", width=15, font=("Arial", 10, "bold"))
     botao_comparar_estoque_inventario = tk.Button(janela_pai, text="Comparar Estoque", width=15, font=("Arial", 10, "bold"))
 
