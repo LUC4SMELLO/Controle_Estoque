a405fa1ce5f7cad2e777c212125a5480bbcd419d
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-10-10 16:52:12 -0300

    começando janela comparar estoque

---
+++ frontend/janelas/inventario/janela_comparar_estoque.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter import messagebox, ttk
+
+from backend.constantes.fontes import LABEL, ENTRY, BOTAO
+
+from backend.binds.configuracao_binds import configurar_binds
+
+
+def criar_janela_comparar_estoque():
+
+    janela_comparar_estoque = tk.Toplevel()
+    janela_comparar_estoque.title("Comparar Estoque")
+    janela_comparar_estoque.geometry("800x600")
+
+    # FRAME ROLÁVEL
+    frame_container = tk.Frame(janela_comparar_estoque)
+    frame_container.pack(fill="both", expand=True)
+
+    canvas = tk.Canvas(frame_container)
+    canvas.pack(side="left", fill="both", expand=True)
+
+    scrollbar = tk.Scrollbar(frame_container, orient="vertical", command=canvas.yview)
+    scrollbar.pack(side="right", fill="y")
+
+    canvas.configure(yscrollcommand=scrollbar.set)
+    canvas.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+
+    # FRAME INTERNO
+    frame_tabela = tk.Frame(canvas)
+    canvas.create_window((0, 0), window=frame_tabela, anchor="nw")
+
+    tk.Label(frame_tabela, text="Código", font=("Arial", 10, "bold"), width=10, height=2, borderwidth=2, relief="ridge").grid(row=0, column=0)
+    tk.Label(frame_tabela, text="Descrição", font=("Arial", 10, "bold"), width=25, height=2, borderwidth=2, relief="ridge").grid(row=0, column=1)
+    tk.Label(frame_tabela, text="Estoque\nContado", font=("Arial", 10, "bold"), width=10, borderwidth=2, relief="ridge").grid(row=0, column=2)
+    tk.Label(frame_tabela, text="Apartado", font=("Arial", 10, "bold"), width=10, height=2, borderwidth=2, relief="ridge").grid(row=0, column=3)
+    tk.Label(frame_tabela, text="Diferença", font=("Arial", 10, "bold"), width=10, height=2, borderwidth=2, relief="ridge").grid(row=0, column=4)
+    tk.Label(frame_tabela, text="Estoque\nSistema", font=("Arial", 10, "bold"), width=10, height=2, borderwidth=2, relief="ridge").grid(row=0, column=5)
+    tk.Label(frame_tabela, text="Status", font=("Arial", 10, "bold"), width=17, height=2, borderwidth=2, relief="ridge").grid(row=0, column=6)
--- frontend/menus/menu_inventario.py
+++ frontend/menus/menu_inventario.py
@@ -3,6 +3,8 @@ import tkinter as tk
 from backend.controladores.menu.menu_controlador import organizar_botoes
 
 from frontend.janelas.inventario.janela_contagem_produtos import criar_janela_contagem_produtos
+from frontend.janelas.inventario.janela_comparar_estoque import criar_janela_comparar_estoque
+
 
 import frontend.menu_state as state
 
@@ -13,7 +15,7 @@ def criar_botao_inventario(janela_pai):
 
     botao_contagem_produtos_inventario = tk.Button(janela_pai, text="Contagem Produtos", width=16, font=("Arial", 10, "bold"), command=criar_janela_contagem_produtos)
     botao_contagem_doces_inventario = tk.Button(janela_pai, text="Contagem Doces", width=15, font=("Arial", 10, "bold"))
-    botao_comparar_estoque_inventario = tk.Button(janela_pai, text="Comparar Estoque", width=15, font=("Arial", 10, "bold"))
+    botao_comparar_estoque_inventario = tk.Button(janela_pai, text="Comparar Estoque", width=15, font=("Arial", 10, "bold"), command=criar_janela_comparar_estoque)
 
     state.botoes_principais["inventario"] = botao_inventario
     state.botoes_submenus["inventario"] = [botao_contagem_produtos_inventario, botao_contagem_doces_inventario, botao_comparar_estoque_inventario,]
