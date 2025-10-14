25439924a70769d9514723d88c06a6f1d7ce5a29
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-10-14 16:53:21 -0300

    classe apartado

---
+++ backend/models/apartado.py
@@ -0,0 +1,78 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from database.banco_dados_apartados import conectar_banco_dados_apartados
+
+class Apartado():
+    def __init__(self, data, codigo_produto, quantidade, motivo):
+        self.data = data
+        self.codigo_produto = codigo_produto
+        self.quantidade = quantidade
+        self.motivo = motivo
+    
+    def inserir_apartado(self):
+
+        conexao = conectar_banco_dados_apartados()
+        cursor = conexao.cursor()
+
+        cursor.execute(
+        """
+        INSERT INTO TabelaApartados (
+        data,
+        codigo_produto,
+        quantidade,
+        motivo
+        )
+        VALUES (?, ?, ?, ?)
+        """,
+            (
+                self.data,
+                self.codigo_produto,
+                self.quantidade,
+                self.motivo
+            )
+        )
+
+    @staticmethod
+    def excluir_apartado(data, codigo_produto, quantidade):
+
+        conexao = conectar_banco_dados_apartados()
+        cursor = conexao.cursor()
+
+        cursor.execute(
+        """
+        DELETE FROM TabelaApartados
+        WHERE data = ? AND codigo_produto = ? AND quantidade = ?
+        """, (data, codigo_produto, quantidade)
+        )
+
+        conexao.commit()
+        conexao.close()
+    
+    @staticmethod
+    def buscar_apartado(data, codigo_produto, quantidade):
+
+        try:
+            conexao = conectar_banco_dados_apartados()
+            cursor = conexao.cursor()
+
+            cursor.execute(
+            """
+            SELECT * FROM TabelaApartados
+            WHERE data = ? AND codigo_produto = ? AND quantidade = ?
+            """, (data, codigo_produto, quantidade)
+            )
+
+            resultado = cursor.fetchone()
+
+            conexao.commit()
+            conexao.close()
+
+            return Apartado(*resultado)
+    
+        except TypeError:
+                return False
+
+
