54ad726d2430232a8c06ce36705bc3068b05f3b6
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-10-19 14:44:15 -0300

    correção referencia banco dados

--- backend/controladores/apartado/historico_apartado.py
+++ backend/controladores/apartado/historico_apartado.py
@@ -5,26 +5,26 @@ def buscar_historico_apartados_back(data="", codigo_produto="", motivo=""):
     conexao = conectar_banco_dados_apartados()
     cursor = conexao.cursor()
 
-    cursor.execute("ATTACH DATABASE 'TabelaProdutos.db' AS TabelaProdutos")
+    cursor.execute("ATTACH DATABASE 'produtos.db' AS produtos")
 
     consulta_sql = """
     SELECT ap.data, ap.codigo_produto, p.descricao, ap.quantidade, ap.motivo
-    FROM TabelaApartados AS ap
-    INNER JOIN TabelaProdutos AS p
+    FROM apartados AS ap
+    INNER JOIN produtos AS p
     ON ap.codigo_produto = p.codigo_produto
     """
     parametros = []
 
     if data:
-        consulta_sql += " AND data = ?"
+        consulta_sql += " AND ap.data = ?"
         parametros.append(data)
     
     if codigo_produto:
-        consulta_sql += " AND codigo_produto = ?"
+        consulta_sql += " AND ap.codigo_produto = ?"
         parametros.append(codigo_produto)
 
     if motivo:
-        consulta_sql += " AND motivo = ?"
+        consulta_sql += " AND ap.motivo = ?"
         parametros.append(motivo)
 
     cursor.execute(consulta_sql, parametros)
