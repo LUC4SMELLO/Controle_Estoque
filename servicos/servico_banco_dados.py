312c234732b560f44b2d4beec43bb2779d03885a
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-10-19 14:45:20 -0300

    adicionando banco dados apartados

--- servicos/servico_banco_dados.py
+++ servicos/servico_banco_dados.py
@@ -2,6 +2,7 @@ from database.banco_dados_produtos import criar_tabela_produtos
 from database.banco_dados_fornecedores import criar_tabela_fornecedores
 from database.banco_dados_nota_fiscal import criar_tabela_nota_fiscal
 from database.banco_dados_itens_nota import criar_tabela_itens_nota_fiscal
+from database.banco_dados_apartados import criar_banco_dados_apartados
 
 def inicializar_bancos_de_dados():
 
@@ -9,3 +10,4 @@ def inicializar_bancos_de_dados():
     criar_tabela_fornecedores()
     criar_tabela_nota_fiscal()
     criar_tabela_itens_nota_fiscal()
+    criar_banco_dados_apartados()
