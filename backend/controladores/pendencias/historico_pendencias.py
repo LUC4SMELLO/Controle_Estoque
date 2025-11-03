8328136a3f7757a9b3cf781dba7c3f62e35eb64c
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-11-03 16:46:50 -0300

    função buscar historico pendencias

---
+++ backend/controladores/pendencias/historico_pendencias.py
@@ -0,0 +1,61 @@
+from database.banco_dados_pendencias import conectar_banco_de_dados_pendencias
+
+from backend.constantes.bancos_dados import TABELA_PENDENCIAS
+
+def buscar_historico_pendencias_back(
+        cupom,
+        data_ocorrencia,
+        codigo_cliente,
+        razao_social,
+        cidade,
+        vendedor,
+        codigo_produto,
+        quantidade
+    ):
+
+    conexao = conectar_banco_de_dados_pendencias()
+    cursor = conexao.cursor()
+
+    consulta_sql = f"""
+    SELECT * FROM {TABELA_PENDENCIAS}
+    """
+    parametros = []
+
+    if cupom:
+        consulta_sql += "AND cupom = ? "
+        parametros.append(cupom)
+
+    if data_ocorrencia:
+        consulta_sql += " AND data_ocorrencia = ?"
+        parametros.append(data_ocorrencia)
+    
+    if codigo_cliente:
+        consulta_sql += " AND codigo_cliente = ?"
+        parametros.append(codigo_cliente)
+
+    if razao_social:
+        consulta_sql += " AND razao_social = ?"
+        parametros.append(razao_social)
+
+    if cidade:
+        consulta_sql += " AND cidade = ?"
+        parametros.append(cidade)
+
+    if vendedor:
+        consulta_sql += " AND vendedor = ?"
+        parametros.append(vendedor)
+
+    if codigo_produto:
+        consulta_sql += " AND codigo_produto = ?"
+        parametros.append(codigo_produto)
+
+    if quantidade:
+        consulta_sql += " quantidade = ?"
+        parametros.append(quantidade)
+
+
+    cursor.execute(consulta_sql, parametros)
+    resultado = cursor.fetchall()
+    conexao.close()
+
+    return resultado
