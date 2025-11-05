85b2d646c53f79c71de3c9d0ea36a89909c0b948
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-11-05 16:21:45 -0300

    docstrings saída entrada

--- backend/controladores/estoque/saida_controlador.py
+++ backend/controladores/estoque/saida_controlador.py
@@ -5,5 +5,22 @@ from backend.models.produto import Produto
 
 
 def saida_produto_back(codigo_produto: str, quantidade_entrada: int):
+    """
+    Da saída na quantidade em estoque de algum produto no banco de dados.
+
+    Parameters
+    ----------
+        codigo_produto
+            O código do produto.
+            
+        quantidade_entrada
+            O valor da quantidade que vai sair.
+            
+    Returns
+    -------
+        None
+        
+    """
+    
     produto = Produto.buscar_produto_por_codigo(codigo_produto.get().strip())
     produto.saida_estoque_produto(int(quantidade_entrada.get().strip()))
