022ffa56a3f01ea0ee9679a73d3ecbf72d9d7c35
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-11-05 16:07:36 -0300

    docstrings entrada estoque

--- backend/controladores/estoque/entrada_controlador.py
+++ backend/controladores/estoque/entrada_controlador.py
@@ -5,5 +5,22 @@ from backend.models.produto import Produto
 
 
 def entrada_produto_back(codigo_produto: str, quantidade_entrada: int):
+    """
+    Da entrada na quantidade em estoque de algum produto no banco de dados.
+
+    Parameters
+    ----------
+        codigo_produto
+            O código do produto.
+            
+        quantidade_entrada
+            O valor da quantidade que vai entrar.
+            
+    Returns
+    -------
+        None
+        
+    """
+
     produto = Produto.buscar_produto_por_codigo(codigo_produto)
     produto.entrada_estoque_produto(int(quantidade_entrada))
