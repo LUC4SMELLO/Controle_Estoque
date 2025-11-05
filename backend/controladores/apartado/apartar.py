979fe2d274dc5a220897b3b6b5157633f4ea6548
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-11-05 15:18:48 -0300

    docstring configuração apartar

--- backend/controladores/apartado/apartar.py
+++ backend/controladores/apartado/apartar.py
@@ -1,6 +1,24 @@
 from backend.models.apartado import Apartado
 
 def apartar_back(data, codigo_produto, quantidade, motivo):
+    """
+    Aparta um produto.
+
+    Parameters
+    ----------
+        data
+            Data atual.
+        codigo_produto
+            Código do produto a ser apartado.
+        quantidade
+            Quantidade do produto a ser apartado.
+        motivo
+            O motivo do apartado.
+
+    Returns
+    ----------
+        None
+    """
 
     novo_apartado = Apartado(data, codigo_produto, quantidade, motivo)
     novo_apartado.inserir_apartado()
