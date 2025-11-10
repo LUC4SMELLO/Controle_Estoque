cba5af0a11cbb2fbab86b0c1237311b1f0a8816e
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-11-10 11:12:16 -0300

    adicionando o processamento de pendências

--- app/main.py
+++ app/main.py
@@ -5,9 +5,11 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from frontend.interface import inicializar_interface
 from servicos.servico_banco_dados import inicializar_bancos_de_dados
+from backend.scripts.processar_pendencias_estoque import processar_pendencias
 
 def main():
     inicializar_bancos_de_dados()
+    processar_pendencias()
     inicializar_interface()
 
 
